456cd3fe9c2db0dcf22eae71fd2c5c9be2033ab8
Jefferson Felipe <felipej2626@gmail.com>
2016-04-24 21:53:24 -0300

    Cont.: Novos Layouts, Alguns ajustes no Login e Facebook, Criação do Tema
    Faltou subir a classe Sistema

--- App/App/src/main/java/br/com/app/Sistema.java
+++ App/App/src/main/java/br/com/app/Sistema.java
@@ -1,5 +1,11 @@
 package br.com.app;
 
+import com.facebook.AccessToken;
+
+import java.util.Arrays;
+
+import br.com.app.activity.R;
+
 /**
  * Created by Wesley on 06/04/2016.
  */
@@ -7,4 +13,5 @@ public class Sistema {
     public static String USER_ID = "";
     public static String SERVIDOR_WS = "192.168.25.10:8090";
     public static String SERVIDOR_WS = "192.168.0.7:8090";
+    public static AccessToken accessToken = null;
 }
